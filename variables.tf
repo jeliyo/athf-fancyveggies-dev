09a5ab5a7fd23246793db476f419e5666296d308
Jasmin Young <20586509+jeliyo@users.noreply.github.com>
2022-01-20 13:08:46 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t2.small"
 }
 
 variable "instance_name" {
